e1a5156e319d972b9bb8b28f7fce000fc868df44
Pedro Travessa <fc59479@alunos.fc.ul.pt>
2023-06-04 22:37:21 +0100

    Retrieved my older commits with myapplaayout and buttons design

--- Semana-14/Kivy2_1.py
+++ Semana-14/Kivy2_1.py
@@ -5,25 +5,33 @@ from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
 from kivy.core.window import Window
 
-Window.size = (900,600)
-class MainBoxLayout(App):
+Window.size = (900, 600)
+
+class Main(App):
     def build(self):
-        box = BoxLayout(orientation='vertical', padding=20, spacing=20)
-        button1 = Button(text='Hi!', size_hint=(None, None), height=50)
-        box.add_widget(button1, height=50)
-
-        buttonRow = BoxLayout(orientation='horizontal', spacing=20, padding=15)
-        button2 = Button(text='Button 1', size_hint=(None, None), height=50)
-        buttonRow.add_widget(button2)
+        main_box = BoxLayout(orientation='vertical', size_hint=(1, 1))
+        button_box = BoxLayout(orientation='vertical', size_hint=(0.1, 0.9))
+        
+        self.buttonFillU = Button(text='', font_size=15, size_hint=(1, 0.1), height=40)
+        self.buttonA = Button(text='A', font_size=15, size_hint=(1, None), height=40)
+        self.buttonB = Button(text='B', font_size=15, size_hint=(1, None), height=40)
+        self.buttonC = Button(text='C', font_size=15, size_hint=(1, None), height=40)
+        self.buttonD = Button(text='D', font_size=15, size_hint=(1, None), height=40)
+        self.buttonFillD = Button(text='', font_size=15, size_hint=(1, 0.1), height=40)
+        button_box.add_widget(self.buttonFillU)
+        button_box.add_widget(self.buttonA)
+        button_box.add_widget(self.buttonB)
+        button_box.add_widget(self.buttonC)
+        button_box.add_widget(self.buttonD)
+        button_box.add_widget(self.buttonFillD)
+        self.label = Label(text=' It`s me! \n PicLib!!', font_size=50, color=(1, 0.5, 0.3, 1))
+        self.buttonMainD = Button(text='bottom bar', font_size=15, size_hint=(1,0.1), height=40)
 
         
-        box.add_widget(buttonRow)
+        main_box.add_widget(button_box)
+        main_box.add_widget(self.label)
+        main_box.add_widget(self.buttonMainD)
         
-        return box
+        return main_box
 
-# class MyApp(App):
-#     def build(self):
-#         return MainBoxLayout()
-
-
-MainBoxLayout().run()        
+Main().run()
