e06ca7f1618d00b6778dbe68ac96298bacbc581b
Pedro Travessa <fc59479@alunos.fc.ul.pt>
2023-06-14 18:01:55 +0100

    Updated main page, added one more class to add color into bottom label Managed the two types of coloring Hex and RGB in comment

--- Projeto/GraphicalUi.py
+++ Projeto/GraphicalUi.py
@@ -3,6 +3,23 @@ from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
+from kivy.graphics import Rectangle, Color
+
+class BrownBoxLayout(BoxLayout):
+    #Class for brown box in label
+    def __init__(self, background_color=(139/255, 69/255, 19/255, 1), **kwargs):
+        super().__init__(**kwargs)
+        with self.canvas.before:
+            Color(*background_color)
+            self.rect = Rectangle()
+            self.rect.pos = self.pos
+            self.rect.size = self.size
+        self.bind(pos=self._update_rect, size=self._update_rect)
+
+    def _update_rect(self, instance, value):
+        self.rect.pos = instance.pos
+        self.rect.size = instance.size
+
 
 class PicLib(App):
     def __init__(self, **kwargs):
@@ -51,16 +68,10 @@ class PicLib(App):
         self.top_row.add_widget(top_row_label)
 
     def create_bottom_row(self):
-        """
-        Creates the bottom row of the application.
-
-        Returns:
-            Nothing, Bottom filler panel.
-        """
-        self.bottom_row = BoxLayout(orientation='horizontal', size_hint=(1, 0.1))
+        self.bottom_row = BrownBoxLayout(orientation='horizontal', size_hint=(1, 0.1))
         bottom_row_label = Label(text='Main Page', font_size=30)
         self.bottom_row.add_widget(bottom_row_label)
-
+        
     def create_main_panel(self):
         """
         Creates the main panel of the application.
@@ -84,11 +95,17 @@ class PicLib(App):
         """
         button_bar = BoxLayout(orientation='vertical', size_hint=(0.1, 1))
 
-        add_tags_button = Button(text='+T', font_size=20)
-        remove_tags_button = Button(text='-T', font_size=20)
-        search_button = Button(text='S', font_size=20)
-        zip_button = Button(text='Zip', font_size=20)
-        rotate_button = Button(text='R90', font_size=20)
+        """add_tags_button = Button(text='+T', font_size=20, background_color=(0.580, 1, 0.855))
+        remove_tags_button = Button(text='-T', font_size=20, background_color=(0.580, 1, 0.855))
+        search_button = Button(text='S', font_size=20, background_color=(0.580, 1, 0.855))
+        zip_button = Button(text='Zip', font_size=20, background_color=(0.580, 1, 0.855))
+        rotate_button = Button(text='R90', font_size=20, background_color=(0.580, 1, 0.855))"""
+
+        add_tags_button = Button(text='+T', font_size=20, background_color='#94FFDA')
+        remove_tags_button = Button(text='-T', font_size=20, background_color='#94FFDA')
+        search_button = Button(text='S', font_size=20, background_color='#94FFDA')
+        zip_button = Button(text='Zip', font_size=20, background_color='#94FFDA')
+        rotate_button = Button(text='R90°', font_size=20, background_color='#94FFDA')
 
         button_bar.add_widget(add_tags_button)
         button_bar.add_widget(remove_tags_button)
@@ -100,7 +117,7 @@ class PicLib(App):
 
 PicLib().run()
 
-"""Temos o layout das 3 caixas necessárias,
+"""Temos o layout das 3 caixas necessárias +1 classe para colocar cor na label,
 os botões das tags estão definidos no entanto
 acho que não temos o Save pretendido não sei se é o das tags ou não,
 nós temos o do self.filename no ImageCollection,
@@ -109,8 +126,8 @@ em relação ao bidding das imagens para darem showcase
 estava a pensar secalhar em grid?Para ficarem todas juntinhas
 sem ocupar diferentes tamanhos no main ficava mais clean
 no entanto não se dá para fazer como estou a pensar,
-aos botões vou lhes meter colors type HEX, é mais universal
-no entanto é irrelevante"""
+aos botões vou lhes meter colors type HEX ou RGB, é mais universal
+ i mean não é preciso fazer a divisão por 255 no entanto é irrelevante"""
 
 """Outra coisa, deves ter de instalar o sdl12 do kivy,
 (pip install "kivy[sdl2]") no terminal """
